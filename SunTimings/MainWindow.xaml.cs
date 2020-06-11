1df13920345b16765e2c4139d6fa9e22d9a0177c
abhinav m <abhinavmusthyala@outlook.com>
2020-06-11 21:47:04 +0530

    Add project files.

---
+++ SunTimings/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+using System.Windows;
+using System.Net;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace SunTimings
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+       private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string url = "https://api.sunrise-sunset.org/json?lat=" + txtLatitude.Text + "&lng=" + txtLongitude.Text;
+                SunOrganisation res = JsonConvert.DeserializeObject<SunOrganisation>(new WebClient().DownloadString(url));
+                if (res.status == "OK")
+                {
+                    lblSunRise.Content = $" SunRise Time is at : {res.results.sunrise} ";
+                    lblSunSet.Content = $" Sunset Time is at : {res.results.sunrise}";
+                }
+            }
+            catch (Exception )
+            {
+                lblSunRise.Content = "Please enter valid values";
+            }
+        }
+
+        public class SunOrganisation
+        {
+            public SunOrganisationResults results { get; set; }
+            public string status { get; set; }
+        }
+
+        public class SunOrganisationResults
+        {
+            public string sunrise { get; set; }
+            public string sunset { get; set; }
+        }
+    }
+}
