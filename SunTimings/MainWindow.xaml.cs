e8ee0d4b507491420da805a8bd10d6bd9193f5f6
abhinav m <abhinavmusthyala@outlook.com>
2020-06-12 14:16:07 +0530

    edit

--- SunTimings/MainWindow.xaml.cs
+++ SunTimings/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Windows;
 using System.Net;
 using Newtonsoft.Json;
 using System.Threading.Tasks;
+using System.Net.Http;
 
 namespace SunTimings
 {
@@ -14,24 +15,27 @@ namespace SunTimings
             InitializeComponent();
         }
 
-       private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        private  void  SubmitButton_Click(object sender, RoutedEventArgs e)
         {
             try
-            {
-                string url = "https://api.sunrise-sunset.org/json?lat=" + txtLatitude.Text + "&lng=" + txtLongitude.Text;
-                SunOrganisation res = JsonConvert.DeserializeObject<SunOrganisation>(new WebClient().DownloadString(url));
-                if (res.status == "OK")
+            {             
+
+                var res = new WebClient().DownloadString("https://api.sunrise-sunset.org/json?lat=" + txtLatitude.Text + "&lng=" + txtLongitude.Text).Split('\"');
+
+                for (int i = 0; i < res.Length; i++)
                 {
-                    lblSunRise.Content = $" SunRise Time is at : {res.results.sunrise} ";
-                    lblSunSet.Content = $" Sunset Time is at : {res.results.sunrise}";
+                    if (res[i] == "sunrise")
+                        lblSunRise.Content = $" SunRise Time is at : {Convert.ToDateTime(res[i + 2]).AddHours(5).AddMinutes(30).ToString("hh:mm tt")}";
+                    if (res[i] == "sunset")
+                        lblSunSet.Content = $" Sunset Time is at : {Convert.ToDateTime(res[i + 2]).AddHours(5).AddMinutes(30).ToString("hh:mm tt")}";
+
                 }
             }
-            catch (Exception )
+            catch (Exception)
             {
                 lblSunRise.Content = "Please enter valid values";
             }
         }
-
         public class SunOrganisation
         {
             public SunOrganisationResults results { get; set; }
